1d28c4cde835895a975a01abcce7b6d438a69ecf
Margot Ballois <ballois.margot@live.fr>
2019-12-08 15:31:56 +0100

    Squelette HTML des boutons burger, bouton FAQ et footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
