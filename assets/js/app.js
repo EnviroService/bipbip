6101913a598a23273378b5be6db7b3e4c94b737f
Margot Ballois <ballois.margot@live.fr>
2019-12-08 17:12:37 +0100

    Réalisation du css des éléments suivants : bouton menu, bouton faq, footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../scss/reset.scss');
 require('../scss/app.scss');
+require('../scss/reset.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
