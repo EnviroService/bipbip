47d2c4f487098422fa9af2e90db505625248ec96
Jean-Roch Masson <jyaire@gmail.com>
2019-12-10 14:36:45 +0100

    Adding contact form : entity, entityType, twig

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("contact/", name="add_message")
+     * @param Request $request
+     * @return Response A response instance
+     */
+    public function add(Request $request): Response
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            return $this->redirectToRoute(
+                'contact_success',
+                [
+                'contact' => $contact,
+                                ]
+            );
+        }
+
+        return $this->render(
+            'contact/contact.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("contact/", name="contact_success")
+     */
+    public function success()
+    {
+        return $this->render(
+            'contact/contact_success.html.twig'
+        );
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('email', TextType::class)
+            ->add('subject', ChoiceType::class, [
+                'choices'  => [
+                    'Je veux proposer un point de collecte' => true,
+                    'Je veux devenir partenaire' => false,
+                    'Autre' => false,
+                ]
+            ])
+            ->add('message', TextAreaType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
