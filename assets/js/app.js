6be0cf29277926b3d75d2e6925af3722766f5bba
Marie Marie <Mariedefre87@gmail.com>
2019-12-10 17:01:10 +0100

    faq 1er commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
+require('../scss/faq.scss');
 require('../scss/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
