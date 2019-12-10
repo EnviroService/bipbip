62ee2895da6efbe3feea6dbc377ee98320918006
Margot Ballois <ballois.margot@live.fr>
2019-12-10 10:55:29 +0100

    premiers essaie page home

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('home', './assets/js/home.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -63,6 +64,6 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
-;
+
 
 module.exports = Encore.getWebpackConfig();
