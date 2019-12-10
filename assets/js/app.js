9438997c20a6249bff849e34b907b8e68d8bc81c
Margot Ballois <ballois.margot@live.fr>
2019-12-10 17:45:20 +0100

    mise en place du carroussel pour l'accueil

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,6 @@ require('../scss/app.scss');
 require('../scss/reset.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+
+require('bootstrap');
