cd45b8def635570c033f347bbf1756bcb91a3558
Margot Ballois <ballois.margot@live.fr>
2019-12-15 13:07:48 +0100

    Mise en forme du menu ouvrant. Il manque encore la transition à faire mais pas de solution pour l'instant.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,21 @@ require('../scss/reset.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
+
+// BURGER MENU
+//story_bubbles = menu burger
+//modal story = menu
+
+const menu_burger = document.querySelector("#menu-burger");
+const menu = document.querySelector("#menu");
+const close_button = document.querySelector(".close-button");
+
+menu_burger.addEventListener('click', function () {
+    menu.classList.toggle('displayed');
+});
+
+close_button.addEventListener('click', function () {
+    menu.classList.toggle('displayed');
+});
+
+
