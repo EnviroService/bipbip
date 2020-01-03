eb8b5bd4afe6087d868a8852547b18fd895283f8
Margot Ballois <ballois.margot@live.fr>
2020-01-03 18:27:03 +0100

    Transfert de l'ancienne branche menuburger sur a branche actuelle.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,23 @@
 require('bootstrap');
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../scss/faq.scss');
 require('../scss/app.scss');
 require('../scss/reset.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
+
+// BURGER MENU
+
+const menu_burger = document.querySelector("#menu-burger");
+const menu = document.querySelector("#menu");
+const close_button = document.querySelector(".close-button");
+
+menu_burger.addEventListener('click', function () {
+    menu.classList.toggle('displayed');
+});
+
+close_button.addEventListener('click', function () {
+    menu.classList.toggle('displayed');
+});
 
