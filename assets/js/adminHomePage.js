f65d60b60a09e3119caf727fe6b7b6d5d53a722a
Marie Marie <Mariedefre87@gmail.com>
2020-01-08 12:01:43 +0100

    change navbar horizontaly and refactor Css, begin navbar Js

--- assets/js/adminHomePage.js
+++ assets/js/adminHomePage.js
@@ -4,26 +4,28 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+const $ = require('jquery');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/admin.scss');
 
-const collecters = document.querySelectorAll('li.collecters');
-const collecterAtionsLister = document.querySelectorAll('p.collecterActionsLister');
-const collecterAtionsAdd = document.querySelectorAll('p.collecterActionsAdd');
+
+const collecters = document.querySelectorAll('.collecters');
+const collecterActionsLister = document.querySelectorAll('.collecterActionsLister');
+const collecterActionsAdd = document.querySelectorAll('.collecterActionsAdd');
 // eslint-disable-next-line no-plusplus
 
 for (let i = 0; i < collecters.length; i++) {
     collecters[i].addEventListener('click', () => {
-        collecterAtionsLister[i].classList.toggle('displayed');
-        collecterAtionsAdd[i].classList.toggle('displayed');
+        collecterActionsLister[i].classList.toggle('displayed');
+        collecterActionsAdd[i].classList.toggle('displayed');
     });
 }
 
-const officialDocuments = document.querySelectorAll('li.officialDocuments');
-const officialDocumentsBc = document.querySelectorAll('p.officialDocumentsBc');
-const officialDocumentsCi = document.querySelectorAll('p.officialDocumentsCi');
+const officialDocuments = document.querySelectorAll('.officialDocuments');
+const officialDocumentsBc = document.querySelectorAll('.officialDocumentsBc');
+const officialDocumentsCi = document.querySelectorAll('.officialDocumentsCi');
 // eslint-disable-next-line no-plusplus
 
 for (let i = 0; i < collecters.length; i++) {
@@ -31,7 +33,14 @@ for (let i = 0; i < collecters.length; i++) {
         officialDocumentsBc[i].classList.toggle('displayed');
         officialDocumentsCi[i].classList.toggle('displayed');
     });
+    // eslint-disable-next-line no-use-before-define
+    $(document).ready(function () {
+        // eslint-disable-next-line no-use-before-define
+        $('$search-user').keyup(function () {
+            // eslint-disable-next-line no-alert
+            alert('ok');
+        });
+    });
 }
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-const $ = require('jquery');
+// any CSS you require will output into a single css file (app.css in this case)
+require('bootstrap');
