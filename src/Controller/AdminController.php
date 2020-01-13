9a5e7979eff0f3b7affd97f7a171a34a35d4fa0a
Marie Marie <Mariedefre87@gmail.com>
2020-01-13 16:24:55 +0100

    searchBar with Ajax and Symfony but array empty

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,5 +24,47 @@ class AdminController extends AbstractController
     public function show(): Response
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="searchBar")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function searchBar(Request $request, EntityManagerInterface $em): Response
+    {
+            $users= $em->getRepository(User::class)->findAll();
+            $result = [];
+        foreach ($users as $user) {
+            array_push($result, $user->getLastname());
+        }
+
+        if ($request->isXmlHttpRequest()) {
+            $result = $request->request->all();
+            dd($result);
+            return new JsonResponse($result);
+        }
+            return $this-> render(
+                'admin/index.html.twig',
+                ["lastname"=>$result]
+            );
+
+        //1return new JsonResponse(['resultSearch'=>$resultSearch]);
+        /*2$response = new Response(json_encode(['resultSearch' => $resultSearch]));
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+*/
+       /*3 if ($request->isXmlHttpRequest()) {
+            $user = $request->query->get('user');
+
+            return $this-> render(
+                'admin/index.html.twig',
+                ['resultSearch' => $resultSearch,
+                'user'=>$user]
+            );
+        }*/
     }
 }
