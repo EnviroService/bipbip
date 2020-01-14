785a27ead7be8d6ebee5ab0b3945b6ff3ea238fc
Jérémy <49756926+Jerem5962@users.noreply.github.com>
2020-01-14 11:43:33 +0100

    Update CollectsController.php

--- src/Controller/CollectsController.php
+++ src/Controller/CollectsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/collects")
+ * @Route("/collect")
  */
 class CollectsController extends AbstractController
 {
