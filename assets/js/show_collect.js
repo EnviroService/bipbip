a34e76a61b55f5d6b8fbc28d4009a80a859669cc
Jerem5962 <jerem-62@outlook.com>
2020-01-16 17:41:02 +0100

    fix bug travis

--- assets/js/show_collect.js
+++ assets/js/show_collect.js
@@ -3,7 +3,6 @@ const showCollect = document.querySelectorAll('a.choice_collect');
 for (let i = 0; i < showCollect.length; i += 1) {
     showCollect[i].addEventListener('click', (e) => {
         if (confirm('Es-tu sûr de vouloir participer à cette collecte ?')) {
-            confirm('Es-tu sûr de vouloir participer à cette collecte ?');
         } else {
             e.preventDefault();
         }
