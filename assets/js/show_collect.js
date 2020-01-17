8799ca6e1b2393812b1900062c1e4c39a379f069
Jerem5962 <jerem-62@outlook.com>
2020-01-17 10:54:46 +0100

    fix bug travis

--- assets/js/show_collect.js
+++ assets/js/show_collect.js
@@ -1,8 +1,6 @@
-/*global event,*/
-/*eslint no-restricted-globals: ["event"]*/
 const showCollect = document.querySelectorAll('a.choice_collect');
 for (let i = 0; i < showCollect.length; i += 1) {
     showCollect[i].addEventListener('click', (e) => {
         confirm('Es-tu sûr de vouloir participer à cette collecte ?') ? '' : e.preventDefault();
-    })
+    });
 }
