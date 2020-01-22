92f4cc555011874b9bdb866ec5c091992c7ed174
Marie Marie <Mariedefre87@gmail.com>
2020-01-22 17:40:43 +0100

    debug

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -29,7 +29,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 /**
- * @Route("/admin")
+ * @Route("/admingit ")
  */
 
 class AdminController extends AbstractController
