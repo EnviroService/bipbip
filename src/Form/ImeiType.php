96ac230ab66f5d191db22acee4fb2c9e0c67a5f1
Jerem5962 <jerem-62@outlook.com>
2020-01-28 17:37:57 +0100

    add shield of imei on collect page

---
+++ src/Form/ImeiType.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Estimations;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImeiType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('imei')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Estimations::class, [
+                'label' => 'Imei'
+            ]
+        ]);
+    }
+}
