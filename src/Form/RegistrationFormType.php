39cac3214df5567f252f8322e86d13feb1c1c7a4
Jean-Roch Masson <jyaire@gmail.com>
2020-03-03 11:06:35 +0100

    Adding AgreeTerm on contact form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -66,7 +66,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Accepter les termes est obligatoire',
                     ]),
                 ],
             ])
