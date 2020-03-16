9e5592a58e8da885b70b6d215e1b530a9f1ecaf7
Jean-Roch Masson <jyaire@gmail.com>
2020-03-16 11:19:26 +0100

    Adding an EventListener to set lastLogin in user table for each connection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Estimations;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
---
+++ src/EventListener/LoginListener.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\EventListener;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use App\Entity\User;
+use DateTime;
+
+class LoginListener
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        // Get the User entity.
+        $user = $event->getAuthenticationToken()->getUser();
+
+        // Update your field here.
+        $user->setSigninDate(new DateTime());
+
+        // Persist the data to database.
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
