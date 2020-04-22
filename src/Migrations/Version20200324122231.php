e01ad66455f70166e08ac42085e71dc4f47706aa
Jean-Roch Masson <jyaire@gmail.com>
2020-04-22 12:07:32 +0200

    error in editing a migration file

--- src/Migrations/Version20200324122231.php
+++ src/Migrations/Version20200324122231.php
@@ -25,6 +25,7 @@ final class Version20200324122231 extends AbstractMigration
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE faq ADD category_id INT NOT NULL');
         $this->addSql('ALTER TABLE faq ADD CONSTRAINT FK_E8FF75CC12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_E8FF75CC12469DE2 ON faq (category_id)');
     }
 
     public function down(Schema $schema) : void
